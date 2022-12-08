f5fc2cc00ea874a169d5143042d5680081605e54
Joy George Kunjikkuru <joymon@gmail.com>
2022-12-07 20:40:51 -0500

    Updated readme.md

--- README.md
+++ README.md
@@ -9,13 +9,18 @@ Demos end to end setup of DNS Zone to AAG to AKS with App gateway Ingress Contro
   - Azure user-assigned managed identity
   - Azure RBAC
   - Azure App Gateway
-- AKS/Kubernetes
+  - AKS
+- Kubernetes
   - AGIC - ingress
   - cert-manager
   - cert-manager
 - SSL
   - LetsEncrypt
 - Automation
+  - Az CLI
+  - HELM Charts for Kubernetes deployment
   - Bicep as IAC
   - PowerShell for scripting
 
+# Credits
+- [https://github.com/paolosalvatori/aks-multi-tenant-agic](https://github.com/paolosalvatori/aks-multi-tenant-agic)
--- commands/00-env-params.ps1
+++ commands/00-env-params.ps1
@@ -9,7 +9,7 @@ $subdomain ="venus" # This will be coming as subdomain.yourdomain.com eg: tenant
 $publicIPName = "aag1-PublicIp" # change if it conflicts
 
 ############################ AAG Specific ##############################
-$aagSubnetName = "aag1-subnet"
+$aagSubnetName = "aag1-subnet" # give without the prefixed vnet/ name as it will concat inside
 $aagName = "aag1"
 $aagMIName = "aag1-ManagedIdentity"
 ############################ AKS Specific ##############################
@@ -19,4 +19,4 @@ $email="<your email>" # used for cluster-issuer
 
 ########################## AKS app deployment ##########################
 $tenantName = "venus" # this is nothing but the subdomain
-$imageRepository = "ealen/echo-server" # eg: mendhak/http-https-echo, hashicorp/http-echo
+$imageRepository = "ealen/echo-server" # Any image that expose http server over 80 eg: mendhak/http-https-echo, hashicorp/http-echo
